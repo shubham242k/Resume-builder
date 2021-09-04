e406b749261aff70f692fd70bd451f7909269dba
Shubham Aggarwal <67068685+shubham242k@users.noreply.github.com>
2021-09-05 00:53:39 +0530

    Update Templates.jsx

--- src/components/Templates.jsx
+++ src/components/Templates.jsx
@@ -15,14 +15,14 @@ let Templates = () =>{
                     history.push("/personaldata");
                     dispatch(templateCreator("A"))
                 }}>
-                    <img src="http://localhost:3000/template1.svg"/>
+                    <img src="https://shubham242k.github.io/Resume-builder/#/template1.svg"/>
                 </div>
                 <div className = "template template2"
                 onClick={()=>{
                     history.push("/personaldata");
                     dispatch(templateCreator("B"))
                 }}>
-                    <img src="http://localhost:3000/template2.svg"/>
+                    <img src="https://shubham242k.github.io/Resume-builder/#/template2.svg"/>
                 </div>
         </div>
         </>
